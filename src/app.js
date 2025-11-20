80e3c53561e17d63da6fd113a2b8faf6160b4009
Amin El-Rifai <amin@users.noreply.github.com>
2025-11-20 18:27:42 +0100

    Added open/close functionality to slide-over navigation

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 import "./scss/main.scss";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerButton = document.querySelector(".header__burger");
+  const nav = document.querySelector(".nav");
+  const closeNav = document.querySelector(".nav__close");
+
+  burgerButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    nav.classList.add("nav--active");
+  });
+
+  closeNav.addEventListener("click", function (e) {
+    e.preventDefault();
+    nav.classList.remove("nav--active");
+  });
+});
