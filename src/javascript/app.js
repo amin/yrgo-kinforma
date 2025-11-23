962bdc3640bc9302d3deca2c42688e4d7e234abf
Amin El-Rifai <amin@users.noreply.github.com>
2025-11-23 20:22:37 +0100

    Discount modal closes on scroll

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -4,6 +4,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const closeNav = document.querySelector(".nav__close");
   const footerList = document.querySelectorAll(".footer__list-heading");
 
+  const discountButton = document.querySelector(".discount__button");
+  const discountContainer = document.querySelector(".discount__container");
+
+  discountButton.addEventListener("click", (e) => {
+    discountContainer.classList.toggle("discount__container--active");
+  });
+
+  window.addEventListener("scroll", (e) => {
+    discountContainer.classList.remove("discount__container--active");
+  });
+
   burgerButton.addEventListener("click", function (e) {
     e.preventDefault();
     nav.classList.add("nav--active");
