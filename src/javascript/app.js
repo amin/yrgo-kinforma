45ae741e0ae63df9f490ec4c7176ff7b8ad81af3
Amin El-Rifai <amin@users.noreply.github.com>
2025-11-23 13:36:35 +0100

    Added responsive footer

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const burgerButton = document.querySelector(".header__burger");
   const nav = document.querySelector(".nav");
   const closeNav = document.querySelector(".nav__close");
+  const footerList = document.querySelectorAll(".footer__list-heading");
 
   burgerButton.addEventListener("click", function (e) {
     e.preventDefault();
@@ -11,5 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
   closeNav.addEventListener("click", function (e) {
     e.preventDefault();
     nav.classList.remove("nav--active");
+  });
+
+  footerList.forEach(function (element) {
+    element.addEventListener("click", function (e) {
+      e.preventDefault();
+      element.nextElementSibling.classList.toggle("footer__list--active");
+    });
   });
 });
